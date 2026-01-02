5bd1d47c868f5495a95cf9de36cb4fda1d9c8842
Tanish Rajput <tanishrajput9@gmail.com>
2026-01-02 22:31:22 +0530

    added browser tools

--- tools/registry.py
+++ tools/registry.py
@@ -4,5 +4,6 @@ from tools.email.gmail import send_email
 from tools.os.system import open_file, list_directory, create_folder, delete_path, run_command
 from tools.web.scraping import scrape_page
 from tools.os.files import read_file, write_file, move_file, copy_file, delete_file
+from tools.os.browser import search_google, search_youtube, open_github, open_stackoverflow, open_url
 
-__all__ = ["search", "get_weather", "send_email","open_file","list_directory","create_folder","delete_path","run_command","scrape_page","read_file","write_file","move_file","copy_file","delete_file"]
+__all__ = ["search", "get_weather", "send_email","open_file","list_directory","create_folder","delete_path","run_command","scrape_page","read_file","write_file","move_file","copy_file","delete_file","open_url","search_google","search_youtube","open_github","open_stackoverflow"]
