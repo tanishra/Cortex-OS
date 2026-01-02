cc87e6dd284db051ad8d03c5c237745512569d50
Tanish Rajput <tanishrajput9@gmail.com>
2026-01-02 22:15:55 +0530

    updated tools registry

--- tools/registry.py
+++ tools/registry.py
@@ -2,5 +2,6 @@ from tools.web.search import search
 from tools.web.weather import get_weather
 from tools.email.gmail import send_email
 from tools.os.system import open_file, list_directory, create_folder, delete_path, run_command
+from tools.web.scraping import scrape_page
 
-__all__ = ["search", "get_weather", "send_email","open_file","list_directory","create_folder","delete_path","run_command"]
+__all__ = ["search", "get_weather", "send_email","open_file","list_directory","create_folder","delete_path","run_command","scrape_page"]
