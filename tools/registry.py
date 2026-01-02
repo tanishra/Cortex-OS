86f87b20c33ec24db7d0122ae58de1d98f3d4a19
Tanish Rajput <tanishrajput9@gmail.com>
2026-01-02 22:23:53 +0530

    added files tools

--- tools/registry.py
+++ tools/registry.py
@@ -3,5 +3,6 @@ from tools.web.weather import get_weather
 from tools.email.gmail import send_email
 from tools.os.system import open_file, list_directory, create_folder, delete_path, run_command
 from tools.web.scraping import scrape_page
+from tools.os.files import read_file, write_file, move_file, copy_file, delete_file
 
-__all__ = ["search", "get_weather", "send_email","open_file","list_directory","create_folder","delete_path","run_command","scrape_page"]
+__all__ = ["search", "get_weather", "send_email","open_file","list_directory","create_folder","delete_path","run_command","scrape_page","read_file","write_file","move_file","copy_file","delete_file"]
