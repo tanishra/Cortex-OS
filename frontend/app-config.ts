14cb5dbfc9e4fd80515a9a44e038f889e5431cd5
Tanish Rajput <tanishrajput9@gmail.com>
2026-01-03 23:29:01 +0530

    added app config

---
+++ frontend/app-config.ts
@@ -0,0 +1,41 @@
+export interface AppConfig {
+  pageTitle: string;
+  pageDescription: string;
+  companyName: string;
+
+  supportsChatInput: boolean;
+  supportsVideoInput: boolean;
+  supportsScreenShare: boolean;
+  isPreConnectBufferEnabled: boolean;
+
+  logo: string;
+  startButtonText: string;
+  accent?: string;
+  logoDark?: string;
+  accentDark?: string;
+
+  // for LiveKit Cloud Sandbox
+  sandboxId?: string;
+  agentName?: string;
+}
+
+export const APP_CONFIG_DEFAULTS: AppConfig = {
+  companyName: 'Cortex-OS',
+  pageTitle: 'Cortex-OS',
+  pageDescription: 'A voice agent that works',
+
+  supportsChatInput: true,
+  supportsVideoInput: true,
+  supportsScreenShare: true,
+  isPreConnectBufferEnabled: true,
+
+  logo: '/lk-logo.svg',
+  accent: '#002cf2',
+  logoDark: '/lk-logo-dark.svg',
+  accentDark: '#1fd5f9',
+  startButtonText: 'Talk to Cortex',
+
+  // for LiveKit Cloud Sandbox
+  sandboxId: undefined,
+  agentName: undefined,
+};
