9135c5b8352fc28e9399c9af5ab8146118cd5167
Tanish Rajput <tanishrajput9@gmail.com>
2026-01-07 19:22:09 +0530

    registered app tools

--- tools/registry.py
+++ tools/registry.py
@@ -5,5 +5,7 @@ from tools.os.system import open_file, list_directory, create_folder, delete_pat
 from tools.web.scraping import scrape_page
 from tools.os.files import read_file, write_file, move_file, copy_file, delete_file
 from tools.os.browser import search_google, search_youtube, open_github, open_stackoverflow, open_url
+from tools.os.apps import open_app, quit_app, focus_app, list_apps
 
-__all__ = ["search", "get_weather", "send_email","open_file","list_directory","create_folder","delete_path","run_command","scrape_page","read_file","write_file","move_file","copy_file","delete_file","open_url","search_google","search_youtube","open_github","open_stackoverflow"]
+
+__all__ = ["search", "get_weather", "send_email","open_file","list_directory","create_folder","delete_path","run_command","scrape_page","read_file","write_file","move_file","copy_file","delete_file","open_url","search_google","search_youtube","open_github","open_stackoverflow","open_app","quit_app","focus_app","list_apps"]
