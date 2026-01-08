43b160baaae3c88bd2109f83c15fe1380db62163
Tanish Rajput <tanishrajput9@gmail.com>
2026-01-08 15:06:57 +0530

    imported search click tools from registry

--- tools/__init__.py
+++ tools/__init__.py
@@ -32,12 +32,15 @@ from tools.registry import (
     close_tab_by_title,
     close_tab_by_index,
     close_all_tabs,
-    close_current_tab
+    close_current_tab,
+    submit_search,
+    smart_click
 )
 
 __all__ = [
     "search", "get_weather", "send_email",
     "open_file", "list_directory", "create_folder", "delete_path", "run_command","scrape_page","read_file","write_file","copy_file","move_file","delete_file",
     "open_url","search_youtube","search_google","open_github","open_stackoverflow","open_app","list_apps","focus_app","quit_app","open_new_tab","scroll_page",
-    "search_on_page","switch_tab","click_element","go_back","go_forward","close_tab_by_title","close_tab_by_index","close_current_tab","close_all_tabs"
+    "search_on_page","switch_tab","click_element","go_back","go_forward","close_tab_by_title","close_tab_by_index","close_current_tab","close_all_tabs","submit_search",
+    "smart_click"
 ]
