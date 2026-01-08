b1bf33efac531025168d5eb934305e755160240a
Tanish Rajput <tanishrajput9@gmail.com>
2026-01-08 15:07:19 +0530

    added click tools for browser

--- tools/registry.py
+++ tools/registry.py
@@ -6,7 +6,7 @@ from tools.web.scraping import scrape_page
 from tools.os.files import read_file, write_file, move_file, copy_file, delete_file
 from tools.os.browser import search_google, search_youtube, open_github, open_stackoverflow, open_url
 from tools.os.apps import open_app, quit_app, focus_app, list_apps
-from tools.browser.control import open_new_tab, go_back, go_forward, search_on_page, click_element, switch_tab, scroll_page, close_all_tabs, close_current_tab, close_tab_by_index, close_tab_by_title
+from tools.browser.control import open_new_tab, go_back, go_forward, search_on_page, click_element, switch_tab, scroll_page, close_all_tabs, close_current_tab, close_tab_by_index, close_tab_by_title, submit_search, smart_click
 
 
-__all__ = ["search", "get_weather", "send_email","open_file","list_directory","create_folder","delete_path","run_command","scrape_page","read_file","write_file","move_file","copy_file","delete_file","open_url","search_google","search_youtube","open_github","open_stackoverflow","open_app","quit_app","focus_app","list_apps","open_new_tab","go_back","go_forward","search_on_page","click_element","switch_tab","scroll_page","close_all_tabs","close_current_tab","close_tab_by_index","close_tab_by_title"]
+__all__ = ["search", "get_weather", "send_email","open_file","list_directory","create_folder","delete_path","run_command","scrape_page","read_file","write_file","move_file","copy_file","delete_file","open_url","search_google","search_youtube","open_github","open_stackoverflow","open_app","quit_app","focus_app","list_apps","open_new_tab","go_back","go_forward","search_on_page","click_element","switch_tab","scroll_page","close_all_tabs","close_current_tab","close_tab_by_index","close_tab_by_title","submit_search","smart_click"]
